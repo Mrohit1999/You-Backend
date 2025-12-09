63f1704a1fb3178a14923f40e9dd1b46efa035a7
Mrohit1999 <90102658+Mrohit1999@users.noreply.github.com>
2025-12-09 08:30:16 +0530

    Fix refresh token validation logic in refreshAccessToken function

--- src/constrollers/user.controller.js
+++ src/constrollers/user.controller.js
@@ -155,7 +155,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken =
     req.cookies.refreshToken || req.body.refreshToken
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized request")
   }
   try {
