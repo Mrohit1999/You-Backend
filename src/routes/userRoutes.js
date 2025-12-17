daa65c8921b2dff531f7858b5b66e3771d04fdb3
Mrohit1999 <90102658+Mrohit1999@users.noreply.github.com>
2025-12-17 14:29:29 +0530

    Refactor userRoutes.js for improved readability and organization of route handlers

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,13 @@
 import { Router } from "express";
-import { loginUser, logoutUser, resisterUser, refreshAccessToken } from "../constrollers/user.controller.js";
+import {
+  loginUser, logoutUser, resisterUser,
+  refreshAccessToken, changeCurrentPassword,
+  getCurrentUser, updateAccountDetails,
+  updateUserAvatar, updateCoverImage, getUserProfile,
+  getWatchHistory
+} from "../constrollers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
-import  {verifyJWT}  from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -17,6 +23,14 @@ router.route("/register")
 router.route("/login").post(loginUser)
 
 //secure routes
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-user-account").patch(verifyJWT, updateAccountDetails)
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
+
 export default router;
